a1118cbb894f57ae8dbfc10270d119b1c3beff13
邵铁军 <shaotiejun@syswin.com>
2018-11-06 17:57:08 +0800

    占坑

---
+++ README.md
@@ -0,0 +1,17 @@
+# VUE-CLI-MUILT-PAGE VUE多页开发构建插件
+## create a new module
+```npm
+npm create [module]
+```
+## develop a module
+```npm
+npm run dev [module] 
+```
+## build a module
+```npm
+npm run build [module] 
+```
+## build all modules
+```npm
+npm run build
+```
---
+++ index.js
@@ -0,0 +1,55 @@
+/**
+ * Service 插件
+ * @param api 一个 PluginAPI 实例
+ * @param projectOptions 一个包含 vue.config.js 内指定的项目本地选项的对象，或者在 package.json 内的 vue 字段。
+ */
+var merge = require('webpack-merge')
+module.exports = (api, projectOptions) => {
+  api.chainWebpack(webpackConfig => {
+    // 通过 webpack-chain 修改 webpack 配置
+    console.log('chainWebpack')
+  })
+
+  api.configureWebpack(webpackConfig => {
+    // 修改 webpack 配置
+    // 或返回通过 webpack-merge 合并的配置对象
+    // console.log(webpackConfig)
+    // webpackConfig.entry = 'src/modules/m1/main.js'
+    // webpackConfig.outputDir = 'dist/m1'
+    // webpackConfig.baseUrl = 'static'
+    return merge(webpackConfig, {
+      entry: 'src/modules/m1/main.js',
+      outputDir: 'dist/m1/static',
+      baseUrl: 'static'
+    })
+  })
+
+  // api.registerCommand('build', args => {
+  //   console.log('vue-cli-mulit-page')
+  //   // 注册 `vue-cli-service build`
+  //   const configA = api.resolveWebpackConfig()
+  //   const modules = args._
+  //   console.log('modules', modules)
+  // })
+
+  // api.registerCommand('serve', args => {
+  //   console.log('vue-cli-mulit-page')
+  //   // 注册 `vue-cli-service build`
+  //   const configA = api.resolveWebpackConfig()
+  //   const module = args._[0]
+  //   console.log('module', module)
+  //   configA.pages = {
+  //     [module]: {
+  //       entry: `src/modules/${module}/main.js`,
+  //       template: `public/index.html`,
+  //       filename: `${module}/index.html`,
+  //     }
+  //   }
+  //   return Promise.resolve(api)
+  //   // console.log(api)
+  // })
+}
+
+// module.exports.defaultModes = {
+//   build: 'production'
+// }
